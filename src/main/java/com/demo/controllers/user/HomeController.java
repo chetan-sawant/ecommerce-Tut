b0ac06778ce258c0c0f818d9bd50ade7f5d6d977
chetan <chetan@xis123>
2019-11-14 17:23:25 +0530

    setup ecommerce

---
+++ src/main/java/com/demo/controllers/user/HomeController.java
@@ -0,0 +1,11 @@
+package com.demo.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class HomeController 
+{
+	
+}
