87b8f9d32d10015ce7132f1186ae0302719ff36c
chetan <chetan@xis123>
2019-11-15 12:58:33 +0530

    template Added (Not Working)

---
+++ src/main/java/com/demo/configuration/TilesConfiguration.java
@@ -0,0 +1,27 @@
+package com.demo.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+public class TilesConfiguration extends WebMvcAutoConfiguration{
+
+	@Bean
+	public UrlBasedViewResolver urlBasedViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setViewClass(TilesView.class);
+		return resolver;
+	}
+	
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer configurer = new TilesConfigurer();
+		configurer.setDefinitions(new String[] {
+				"/WEB-INF/tiles/tiles_user.xml"
+		});
+		return configurer;
+	}
+}
--- src/main/java/com/demo/controllers/user/HomeController.java
+++ src/main/java/com/demo/controllers/user/HomeController.java
@@ -2,10 +2,14 @@ package com.demo.controllers.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("home")
 public class HomeController 
 {
-	
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+		return "home.index";
+	}
 }
